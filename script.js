1700ffea3793ece13b319fd99cf31b2888a987c4
Senih <40578755+senihucar@users.noreply.github.com>
2023-02-07 17:48:47 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+function initMap() {
+  new google.maps.Map(document.getElementById("map"), {
+    mapId: "54ec69962ab64cbf",
+    center: { lat: 37.579609, lng: 36.946812 },
+    zoom: 12,
+  });
+}
+
+window.initMap = initMap;
